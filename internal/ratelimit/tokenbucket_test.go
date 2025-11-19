3328c6cf856cb13696f8d2023fc6ce4585c052b8
Gomonov <y.gomonov@yandex.ru>
2025-11-20 01:35:06 +0300

    rate_limit

--- internal/ratelimit/tokenbucket_test.go
+++ internal/ratelimit/tokenbucket_test.go
@@ -13,14 +13,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTokenBucket_BasicFunctionality(t *testing.T) {
+func setupTest(t *testing.T) (*redis.Client, func()) {
+	t.Helper()
+
 	mr, err := miniredis.Run()
 	require.NoError(t, err)
-	defer mr.Close()
 
 	client := redis.NewClient(&redis.Options{
 		Addr: mr.Addr(),
 	})
+
+	cleanup := func() {
+		client.Close()
+		mr.Close()
+	}
+
+	return client, cleanup
+}
+
+func TestTokenBucket_BasicFunctionality(t *testing.T) {
+	client, cleanup := setupTest(t)
+	defer cleanup()
 
 	bucket := NewTokenBucket(client, "test:basic", 3, 60)
 	ctx := context.Background()
@@ -39,20 +52,15 @@ func TestTokenBucket_BasicFunctionality(t *testing.T) {
 }
 
 func TestTokenBucket_RefillLogic(t *testing.T) {
-	mr, err := miniredis.Run()
-	require.NoError(t, err)
-	defer mr.Close()
-
-	client := redis.NewClient(&redis.Options{
-		Addr: mr.Addr(),
-	})
+	client, cleanup := setupTest(t)
+	defer cleanup()
 
 	bucket := NewTokenBucket(client, "test:refill", 10, 10)
 	ctx := context.Background()
 
 	// Симулируем: 0 токенов 5 секунд назад
 	initialTime := time.Now().Unix() - 5
-	err = bucket.saveAllowance(ctx, 0, initialTime)
+	err := bucket.saveAllowance(ctx, 0, initialTime)
 	require.NoError(t, err)
 
 	// Должно быть 5 токенов (5 * 10 / 10)
@@ -72,26 +80,29 @@ func TestTokenBucket_RefillLogic(t *testing.T) {
 }
 
 func TestTokenBucket_IndependentBuckets(t *testing.T) {
-	mr, err := miniredis.Run()
-	require.NoError(t, err)
-	defer mr.Close()
-
-	client := redis.NewClient(&redis.Options{
-		Addr: mr.Addr(),
-	})
+	client, cleanup := setupTest(t)
+	defer cleanup()
 
 	bucket1 := NewTokenBucket(client, "test:bucket1", 2, 60)
 	bucket2 := NewTokenBucket(client, "test:bucket2", 2, 60)
 	ctx := context.Background()
 
-	// Исчерпываем bucket1
-	bucket1.Allow(ctx)
-	bucket1.Allow(ctx)
-	allowed, _ := bucket1.Allow(ctx)
-	assert.False(t, allowed)
+	// Исчерпываем bucket1 с проверкой ошибок
+	allowed, err := bucket1.Allow(ctx)
+	require.NoError(t, err)
+	assert.True(t, allowed)
+
+	allowed, err = bucket1.Allow(ctx)
+	require.NoError(t, err)
+	assert.True(t, allowed)
+
+	// Третий запрос должен быть отклонен
+	allowed, err = bucket1.Allow(ctx)
+	require.NoError(t, err)
+	assert.False(t, allowed, "Bucket1 should be exhausted")
 
 	// Bucket2 все еще работает
 	allowed, err = bucket2.Allow(ctx)
 	assert.NoError(t, err)
-	assert.True(t, allowed)
+	assert.True(t, allowed, "Bucket2 should still work")
 }
